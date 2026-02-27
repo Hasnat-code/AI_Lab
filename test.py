c309ef08dd9242f18abaaf7a91f534dc72eb18b9
Hasnat-code <hasnatimtiaz949@gmail.com>
2026-02-27 10:21:57 +0500

    Initial commit

--- test.py
+++ test.py
@@ -2,16 +2,14 @@ Unit_Consumed=int(input("Enter the unit consumed: "))
 if Unit_Consumed>=100:
     rateperunit=5
     total_Bill=Unit_Consumed*rateperunit
-    print("The unit consumed is ",Unit_Consumed," rate per unit: ",rateperunit," total bill: ",total_Bill)
 elif Unit_Consumed>100 and Unit_Consumed<=200:
     rateperunit=7
-    total_Bill=Unit_Consumed*rateperunit
-    print("The unit consumed is ", Unit_Consumed, " rate per unit: ", rateperunit, " total bill: ", total_Bill)
+
 elif Unit_Consumed>200 and Unit_Consumed<=300:
     rateperunit=10
-    total_Bill=Unit_Consumed*rateperunit
-    print("The unit consumed is ", Unit_Consumed, " rate per unit: ", rateperunit, " total bill: ", total_Bill)
 elif Unit_Consumed>300:
     rateperunit=15
-    total_Bill=Unit_Consumed*rateperunit
-    print("The unit consumed is ", Unit_Consumed, " rate per unit: ", rateperunit, " total bill: ", total_Bill)
+total_Bill = Unit_Consumed * rateperunit
+print("The unit consumed is ", Unit_Consumed, " rate per unit: ", rateperunit, " total bill: ", total_Bill)
+
+
