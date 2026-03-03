c94d48b720c82dff3cf02d34ea64bc61adc06a25
Muhammad Hasnat Imtiaz <hasnatimtiaz949@gmail.com>
2026-03-03 06:38:20 +0500

    Fix leap year logic and formatting in Task2.py

--- Task2.py
+++ Task2.py
@@ -1,7 +1,7 @@
 while True:
-
-        Year=int(input("Enter the Year: "))
-        if Year%4==0 or (Year%100==0 and Year%400==0):
-            print(f"The year {Year}  is leap year")
-        else:
-            print(f"{Year} is not leap year")
+    Year = int(input("Enter the Year: "))
+    
+    if (Year % 4 == 0 and Year % 100 != 0) or (Year % 400 == 0):
+        print(f"The year {Year} is leap year")
+    else:
+        print(f"{Year} is not leap year")
